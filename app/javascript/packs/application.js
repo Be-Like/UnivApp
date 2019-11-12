7bcd5b39bb7b3093e56c0ee8eeb44a51abe80686
Jake Simpson <simpson.jacob.barry@gmail.com>
2019-11-12 07:46:56 -0500

    Section 3 Lecture 42: Add dropdown feature and hamburger menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require('@rails/ujs').start();
-require('turbolinks').start();
-require('@rails/activestorage').start();
-require('channels');
+require("@rails/ujs").start();
+require("jquery");
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-import 'materialize-css/dis/js/materialize';
+import "materialize-css/dist/js/materialize";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +17,8 @@ import 'materialize-css/dis/js/materialize';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on("turbolinks:load", function() {
+  $(".dropdown-trigger").dropdown();
+  $(".sidenav").sidenav();
+});
