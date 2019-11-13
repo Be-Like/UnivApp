e9dbea745708d497333e75128ffa98f448504103
Jake Simpson <simpson.jacob.barry@gmail.com>
2019-11-13 08:10:10 -0500

    Section 3 Lecture 59: Create and destroy sessions - add auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  # this (vvv) makes the two methods accessible to all the views
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    # the pipes represent "Memorization". It will check if current user already 
+    # exists and then skip the db query if it does.  
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    !!current_user # returns boolean dependng on current user
+  end
+  
 end
